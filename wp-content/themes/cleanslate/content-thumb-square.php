99f88dab514bcab89686942e0df1f9746d2d3e9c
Saki Sato <sakiisato@gmail.com>
2013-07-22 17:07:17 -0400

    changed image quesry to common get image fucntion

--- wp-content/themes/cleanslate/content-thumb-square.php
+++ wp-content/themes/cleanslate/content-thumb-square.php
@@ -19,45 +19,9 @@
 </figcaption>
 
 <figure class="post-thumb">
-    
     <?php
-        // BEGIN Get thumbnail
-        $args = array(
-            'post_parent' => $post->ID,
-            'post_type' => 'attachment',
-            'post_mime_type' => 'image',
-            'orderby' => 'menu_order',
-            'order' => 'ASC'
-        );
-        
-        // Get image attachments
-        $attachments = get_children( $args );
-        
-        // Featured Image
-        $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID, 'thumbnail') );
-        
-        if ( $attachments ) :
-            
-            if ( has_post_thumbnail($post->ID) ) :
-                // Use Featured Image URL
-                $thumb = $featImg;
-                $thumbUrl = $thumb[0];
-            else :
-                // Use only first value in array
-                $attachment = array_shift( $attachments );
-            
-                // Get thumbnail URL
-                $thumb = wp_get_attachment_image_src( $attachment->ID, 'thumbnail' );
-                $thumbUrl = $thumb[0];
-            endif;
-        else :
-        
-            $thumbUrl = get_bloginfo('template_directory') . "/images/thumb-browse.jpg";
-        
-        endif;
-        // END Get Thumbnail
+        $thumb = get_thumbnail_custom($post->ID, 'browse-thumbnail');
     ?>
-    
-    <a href="<?php the_permalink(); ?>" style="background: url('<?php echo $thumbUrl; ?>') no-repeat 0 0;"></a>
+    <a href="<?php the_permalink(); ?>" style="background: url('<?php echo $thumb; ?>') no-repeat 0 0;"></a>
         
 </figure>

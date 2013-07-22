0abb6667dea43844b018e670dfa66054fff2a9a3
Saki Sato <sakiisato@gmail.com>
2013-07-22 16:59:28 -0400

    page template with iamge query for attached or features images using standard function

--- wp-content/themes/cleanslate/content-page.php
+++ wp-content/themes/cleanslate/content-page.php
@@ -9,18 +9,56 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     
+    <?php
+        // Define args to get attachments
+        $args = array(
+            'post_parent' => $post->ID,
+            'post_type' => 'attachment',
+            'post_mime_type' => 'image',
+            'orderby' => 'menu_order',
+            'order' => 'ASC'
+        );
+        
+        // Get image attachments
+        $attachments = get_posts( $args );
+        
+        // Featured Image
+        $featImg = wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'page' );
+        
+        if ( has_post_thumbnail($post->ID) || $attachments ) :
+            $noImageClass = '';
+        else :
+            $noImageClass = 'no-image';
+        endif;
+    ?>
+    
     <div id="text" class="text-container <?php echo $noImageClass; ?>">
         <?php the_content(); ?>
     </div>
     
-        <figure>
-            <img src="<?php echo get_thumbnail_custom($post->ID); ?>" width="350" height="" alt="About me" />
-        </figure>
-        <figcaption>
-            <?php
-                // Insert image description
-                echo $attachment->post_content;
-            ?>
-        </figcaption>
+    <?php
+        if ( has_post_thumbnail($post->ID) ) :
+    ?>
+            <figure>
+                <?php echo $featImg; ?>
+            </figure>
+    <?php
+        
+        elseif ( $attachments ) :
+            // Insert images uploaded to post
+            foreach ( $attachments as $attachment ) {
+                
+                $image = wp_get_attachment_image( $attachment->ID, 'page' );
+                $imageUrl = wp_get_attachment_image_src( $attachment->ID, 'page' );
+    ?>
+            <figure>
+                <?php echo $image; ?>
+            </figure>
+    <?php
+              }
+        else :
+            // do nothing
+        endif;
+    ?>
     
 </article>

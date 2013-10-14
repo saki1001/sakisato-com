48ee155fc4f2436c7fd43c60030683e73617e336
Saki Sato <sakiisato@gmail.com>
2013-10-14 18:58:03 -0400

    added fitvid script, removed search script

--- wp-content/themes/cleanslate/header.php
+++ wp-content/themes/cleanslate/header.php
@@ -46,8 +46,8 @@
         <?php wp_head(); ?>
         
         <?php /*Custom JS Files*/ ?>
+            <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.js" type="text/javascript"></script>
             <script src="<?php echo get_template_directory_uri(); ?>/js/main.js" type="text/javascript"></script>
-            <script src="<?php echo get_template_directory_uri(); ?>/js/search-ajax.js" type="text/javascript"></script>
         <?php
             if( is_home() ) {
         ?>
